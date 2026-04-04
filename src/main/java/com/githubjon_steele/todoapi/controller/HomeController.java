5e06653aac314a585401667c43c4800d06675bbd
Jon Steele <jonwhsteele@gmail.com>
2026-04-03 23:44:46 -0700

    Adds home controller handling

---
+++ src/main/java/com/githubjon_steele/todoapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.githubjon_steele.todoapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Todo API is running";
+	}
+}
